fe42b1b3bae4b781f7aeae4b913343674fb1cd67
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-08 13:10:28 +0530

    Add files via upload

---
+++ app64.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    console.log("hello");
+    console.log("hello");
+    return a+b;
+    console.log("hello");
+    console.log("hello");
+}
+
+function isAdult(age) {
+    if(age >= 18) {
+        return "adult";
+    }
+    else {
+        return "not adult";
+    }
+    console.log("bye bye");
+
+}
+
+
